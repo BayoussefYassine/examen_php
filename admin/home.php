c16fe9cc0bf0f943ad7940ba8b72ba49c4cf4303
BayoussefYassine <yassine.bayoussef97@gmail.com>
2021-03-09 03:04:15 +0100

    test

--- admin/home.php
+++ admin/home.php
@@ -1,1 +1,12 @@
+<?php
+
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("Location: login.php");
+        exit();
+    }
+
+
+?>
+
 <h1>Admin page</h1>
--- scolarite/home.php
+++ scolarite/home.php
@@ -1,1 +1,12 @@
+<?php
+
+    session_start();
+    if(!isset($_SESSION["username"])){
+        header("Location: login.php");
+        exit();
+    }
+
+
+?>
+
 <h1>Scolarité page</h1>
